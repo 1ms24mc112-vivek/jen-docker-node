3cee51b6e4c2d77851f85db554ef3c64d4c326b6
vivek shet <1ms24mc112@gmail.com>
2026-01-10 10:20:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Node.js App Automated using Jenkins & Docker ');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
